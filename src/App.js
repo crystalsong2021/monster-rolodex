e7c8d77de9bdee4c4703894ed37a6ffe14bc9e77
crystal <2022csong@gmail.com>
2022-05-13 09:03:49 -0700

    render

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1> hello </h1>
+class App extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      name : 'Crystal'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         <h1> hello </h1>
+        <p>Hi {this.state.name}</p>
+         <button> Change Name</button>
+        </header>
 
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
+
 }
 
 export default App;
